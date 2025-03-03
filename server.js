aaa09e2f998f5a4ae328a3a2b5b63fda079ad437
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-03 14:12:15 -0800

    Add link to /home and display Home Page

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const app = express();
-
 // Define a route for the root URL "/"
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Hello, World!</h1>"); //status is optional, but recommended
@@ -10,4 +8,8 @@ app.get("/", (req, res) => {
 // Start the server on port 8000
 app.listen(8000, () => {
   console.log("Server running at http://127.0.0.1:8000/");
+});
+
+app.get("/home", (req, res) => {
+  res.send("<h1>Home Page</h1>");
 });
